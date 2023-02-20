81c7c7935bc17b1a28874dd6cb810819387239f8
xiangnan <xiangnan@webuy.ai>
2023-02-20 16:36:09 +0800

    init

---
+++ product-service/src/main/java/icu/funkye/service/impl/ProductServiceImpl.java
@@ -0,0 +1,33 @@
+package icu.funkye.service.impl;
+
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import icu.funkye.entity.Product;
+import icu.funkye.mapper.ProductMapper;
+import icu.funkye.service.IProductService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> implements IProductService {
+
+    @Override
+//    @Transactional
+    public Boolean reduceStock(Integer id, Integer sum) {
+        return update(Wrappers.<Product>lambdaUpdate().eq(Product::getId, id).ge(Product::getStock, 1)
+            .setSql("stock=stock-" + sum));
+    }
+
+    @Override
+//    @Transactional
+    public Object addStock(Integer id, Integer sum) {
+        Product product = getOne(Wrappers.<Product>query().eq("id", id).last("for update"));
+        product.setStock(product.getStock() + sum);
+        if (updateById(product)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
